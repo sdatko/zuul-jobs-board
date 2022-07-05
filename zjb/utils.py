2210fa1f60a7084e8150d389fabb552c82b9e646
Szymon Datko <sdatko@redhat.com>
2022-07-05 22:01:19 +0200

    Add match function
    This commit introduces a function that implements awk-like matching.
    The implementation comes from Znoyder project, where I originally
    introduced it a couple of weeks ago with a pull request [1]
    as part of a bigger change.
    
    [1] https://github.com/rhos-infra/znoyder/pull/38

---
+++ zjb/utils.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import re
+
+
+def match(string: str, specifier: str) -> bool:
+    '''Function checks if a given string is matched by a given specifier.
+
+    The match is performed in the awk-inspired fashion:
+    – if the specifier starts and ends with the forward slash (/), e.g. /foo/,
+      then the content between slashes is treated as regular expression,
+    – otherwise it is a value that should fully match the input string.
+
+    Parameters
+    ----------
+    string : str
+        The string that should be tested against specifier.
+    specifier : str
+        The expected value or regular expression to be matched against.
+
+    Returns
+    -------
+    matched : bool
+        True if given string matches the specifier, False otherwise.
+
+    Examples
+    --------
+    >>> match('foobar', 'foobar')
+    True
+    >>> match('foobar', 'foo')
+    False
+    >>> match('foobar', '/foo/')
+    True
+    '''
+
+    if specifier.startswith('/') and specifier.endswith('/'):
+        regex = re.compile(specifier[1:-2])
+        return bool(regex.search(string))
+    else:
+        regex = re.compile(specifier)
+        return bool(regex.fullmatch(string))
