d041b3236d6bc72a6fe5badb399eabc99c1f5902
Szymon Datko <szymon.datko@gmail.com>
2022-07-08 02:15:47 +0200

    Parallelize puller and server
    This commit introduces multiprocessing to the cli module
    so both the puller and server modules can be run at the same
    time and immediately provide some results.

--- zjb/cli.py
+++ zjb/cli.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import multiprocessing
+
 from zjb import puller
 from zjb import server
 
 
-def main(argv=None) -> None:
-    puller.main()
-    server.main()
+def main() -> None:
+    puller_process = multiprocessing.Process(target=puller.main)
+    server_process = multiprocessing.Process(target=server.main)
+
+    puller_process.start()
+    server_process.start()
+
+    puller_process.join()
+    server_process.join()
