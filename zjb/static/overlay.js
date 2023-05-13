270c668def98d46b0804b5fdab1dd1cb4f5ce347
Szymon Datko <szymon.datko@gmail.com>
2023-05-13 02:07:51 +0200

    Move JavaScript code to static files
    Similarly to CSS before, this commit splits single
    big JavaScript code block into smaller functions,
    stored in separate files – to make the maintenance
    easier in the future. Minor optimizations were
    added as well.

---
+++ zjb/static/overlay.js
@@ -0,0 +1,32 @@
+function open_overlay(event) {
+    let overlay = event.currentTarget.nextElementSibling;
+    overlay.classList.add('visible');
+
+    event.preventDefault();
+    event.stopPropagation();
+    return false;
+}
+
+function close_overlay() {
+    let overlays = document.querySelectorAll('div.overlay.visible');
+    for(const overlay of overlays) {
+        overlay.classList.remove('visible');
+        overlay.firstElementChild.firstElementChild.reset();
+    }
+}
+
+function bind_links_to_overlays() {
+    let links = document.querySelectorAll('main table td a.status');
+    for(const link of links) {
+        link.addEventListener('click', open_overlay);
+    }
+}
+
+function key_pressed(event) {
+    const isNotCombinedKey = !(event.ctrlKey || event.altKey || event.shiftKey);
+    if (event.key === 'Escape' && isNotCombinedKey) {
+        close_overlay();
+    }
+}
+
+document.addEventListener('keydown', key_pressed);
